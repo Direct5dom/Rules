d17c85cf2e7b06091a7672570ca33b88941618b2
SI Xiaolong <SIXiaolong_GitHub@outlook.com>
2024-04-28 16:49:13 +0800

    init

---
+++ clear-and-commit.bat
@@ -0,0 +1,13 @@
+rmdir .git /s/q
+
+git init
+
+git checkout -b main
+
+git add -A
+
+git commit -m "init"
+
+git remote add origin https://github.com/Direct5dom/Rules.git
+
+git push -f origin main
